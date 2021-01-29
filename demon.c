ebfb7f58505d725774e64b18718e5ead22a79037
Jelnes <78155293+Jelnes@users.noreply.github.com>
2021-01-29 13:39:37 +0100

    Add files via upload

---
+++ demon.c
@@ -0,0 +1,43 @@
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main()
+{
+	if (0 == fork())
+	{
+		setsid();
+
+		signal(SIGHUP, SIG_IGN);
+
+		if (0 == fork())
+		{
+			close(0);
+			close(1);
+			close(2);
+
+			int file = open("logg.txt", O_WRONLY|O_CREAT, 0666);
+        		dup2(file, 2);
+
+			char *args[]={"./tjener", NULL};
+			execvp(args[0], args);
+
+		}
+		else
+		{
+			exit(0);
+		}
+
+	}
+	else
+	{
+		exit(0);
+	}
+	return 0;
+}
