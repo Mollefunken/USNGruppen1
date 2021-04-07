fe8ad1a12825cd6c2d8f201ee147d475d4590ca9
Peder Ellefsen <78155209+pedellefsen@users.noreply.github.com>
2021-04-07 13:08:54 +0200

    Add files via upload

---
+++ Peder/create_database.sql
@@ -0,0 +1,38 @@
+DROP TABLE IF EXISTS Bruker;
+
+CREATE TABLE Bruker (
+	epostadresse text NOT NULL,
+	passordhash text NOT NULL,
+	fornavn text NOT NULL,
+	etternavn text NOT NULL,
+	PRIMARY KEY(epostadresse)
+);
+
+DROP TABLE IF EXISTS Sesjon;
+
+CREATE TABLE Sesjon (
+	sesjonsID integer NOT NULL,
+	epostadresse text NOT NULL,
+	PRIMARY KEY(sesjonsID),
+	FOREIGN KEY(epostadresse)
+		REFERENCES Bruker(epostadresse)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	UNIQUE(sesjonsID)
+);
+
+DROP TABLE IF EXISTS Dikt;
+
+CREATE TABLE Dikt (
+	diktID integer NOT NULL,
+	dikt text NOT NULL,
+	epostadresse text NOT NULL,
+	PRIMARY KEY(diktID),
+	FOREIGN KEY(epostadresse)
+		REFERENCES Bruker(epostadresse)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	UNIQUE(diktID)
+);
+
+PRAGMA foreign_keys = ON;
